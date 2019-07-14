1fa34c4fcd738cd18649cc6015a43762b5592a6a
oscarkremer <oscar.s.kremer@hotmail.com>
2019-07-14 14:42:24 -0300

    added-population

--- src/api/train.py
+++ src/api/train.py
@@ -6,10 +6,8 @@ import math
 import numpy as np
 from functools import partial
 from multiprocessing import Pool
-from src.models import Node
+from src.models import Population
 from src.utils import Mentor, Polinomy, integration
-
-
 
 
 
@@ -18,6 +16,5 @@ if __name__=="__main__":
     time = 2
     theta_i = [0, 0, 0, 0, 0]
     theta_f = [3.1415, 3.1415, 3.1415, 3.1415, 3.1415]
-    for i in range(100):
-        test = Node(theta_i, theta_f, time, steps)
-        print(test.dist)    
+    test = Population(10, 3, 0.9, 0.05, theta_i, theta_f, time, steps)
+        
--- src/models/node/node.py
+++ src/models/node/node.py
@@ -1,67 +1,53 @@
-import numpy as np    
+import random
+import numpy as np
+from functools import partial
+from src.utils import Mentor, Polinomy
+from src.utils.numerical import integration, create_angles
 
 
 class Node:
     def __init__(self, theta_i, theta_f, time, steps):
-        thetas, deltas = self.create_angles(theta_i, theta_f, time, steps)
-        
-    def create_angles(self, theta_i, theta_f, time, steps):
-        thetas, deltas = [], []
-        for j in range(5):
-            theta = []
-            for i in range(steps-1):
-                theta.append((theta_f[j] - theta_i[j])*random.random() + theta_i[j])
-            theta.append(theta_f[j])
-            if theta_i[j] < theta_f[j]:
-                theta.sort()
-            else:
-                theta.sort(reverse=True)    
-            deltas.append(derivative(theta, time/steps))
-            thetas.append(theta)
-        return thetas, deltas
-
-    def derivative(theta, delta):
-        derivative = []
-        for i in range(len(theta)):
-            if i == 0:
-                derivative.append(0)
-            else:
-                derivative.append((theta[i] - theta[i-1])/delta)
-        return derivative
-
-    def execute_grid_search(robot):
-        angles = create_angles()
-        pool = Pool(multiprocessing.cpu_count())
-        results = pool.map(partial(find_colision), angles)
-        pool.close()
-        pool.join()
-        print(results)
+        times, thetas, omegas = [], [], []
+        for i in range(5):
+            deltas, delta_thetas, delta_omegas = create_angles(theta_i[i], theta_f[i], time, steps)
+            times.append(deltas)
+            thetas.append(delta_thetas)
+            omegas.append(delta_omegas)
+        self.joint1 = [times[0], thetas[0], omegas[0]]
+        self.joint2 = [times[1], thetas[1], omegas[1]]
+        self.joint3 = [times[2], thetas[2], omegas[2]]
+        self.joint4 = [times[3], thetas[3], omegas[3]]
+        self.joint5 = [times[4], thetas[4], omegas[4]]
+        self.dist_calc(steps, theta_i, time)
 
 
-    if __name__=="__main__":
-        for i in range(1000):
-            time = 4
-            theta_i = [0, 0, 0, 0, 0]
-            theta_f = [3.1415, 3.1415, 3.1415, 3.1415, 3.1415]
-            thetas, delta_thetas = create_angles(theta_i, theta_f, time, 1000)
-            mentor = Mentor()
-            dist_4, dist_3, dist_2, dist_1 = 0, 0, 0, 0
+    def dist_calc(self, steps, theta_i, time):
+        mentor = Mentor()
+        angles, points, polynomies = [], [], []
+        for j in range(steps-1):
+            sub_polynomies = []
+            sub_polynomies.append(Polinomy(self.joint1[0][j], self.joint1[0][j+1], self.joint1[1][j], self.joint1[1][j+1], self.joint1[2][j], self.joint1[2][j+1], number = np.ceil(1000*(self.joint1[0][j+1] - self.joint1[0][j])/time)))
+            sub_polynomies.append(Polinomy(self.joint2[0][j], self.joint2[0][j+1], self.joint2[1][j], self.joint2[1][j+1], self.joint2[2][j], self.joint2[2][j+1], number = np.ceil(1000*(self.joint2[0][j+1] - self.joint2[0][j])/time)))
+            sub_polynomies.append(Polinomy(self.joint3[0][j], self.joint3[0][j+1], self.joint3[1][j], self.joint3[1][j+1], self.joint3[2][j], self.joint3[2][j+1], number = np.ceil(1000*(self.joint3[0][j+1] - self.joint3[0][j])/time)))
+            sub_polynomies.append(Polinomy(self.joint4[0][j], self.joint4[0][j+1], self.joint4[1][j], self.joint4[1][j+1], self.joint4[2][j], self.joint4[2][j+1], number = np.ceil(1000*(self.joint4[0][j+1] - self.joint4[0][j])/time)))
+            sub_polynomies.append(Polinomy(self.joint5[0][j], self.joint5[0][j+1], self.joint5[1][j], self.joint5[1][j+1], self.joint5[2][j], self.joint5[2][j+1], number = np.ceil(1000*(self.joint5[0][j+1] - self.joint5[0][j])/time)))
+            polynomies.append(sub_polynomies)
+        angle_1 = np.array([theta_i[0]])
+        angle_2 = np.array([theta_i[1]])
+        angle_3 = np.array([theta_i[2]])
+        angle_4 = np.array([theta_i[3]])
+        angle_5 = np.array([theta_i[4]])
+        for i in range(steps-1):
+            angle_1 = np.concatenate((angle_1, polynomies[i][0].thetas[1:])) 
+            angle_2 = np.concatenate((angle_2, polynomies[i][1].thetas[1:])) 
+            angle_3 = np.concatenate((angle_3, polynomies[i][2].thetas[1:])) 
+            angle_4 = np.concatenate((angle_4, polynomies[i][3].thetas[1:])) 
+            angle_5 = np.concatenate((angle_5, polynomies[i][4].thetas[1:])) 
 
-            for i in range(1000):
-                dist_4+=6*delta_thetas[3][i]*(time/1000)
-
-            for i in range(1000):
-                dist_3+=delta_thetas[2][i]*np.sqrt(36 - 12*mentor.a[3]*np.sin(thetas[3][i])+ np.power(mentor.a[3], 2))*(time/1000)
-
-            for i in range(1000):
-                dist_2+=delta_thetas[1][i]*np.sqrt(36+np.power(mentor.a[2], 2)+np.power(mentor.a[3], 2)+2*mentor.a[2]*mentor.a[3]*np.cos(thetas[2][i])-12*mentor.a[2]*np.sin(thetas[3][i] + thetas[2][i])
-                -12*mentor.a[3]*np.sin(thetas[3][i]))*(time/1000)
-
-            for i in range(1000):
-                dist_1+=delta_thetas[0][i]*np.sqrt(np.power(mentor.a[2]*np.cos(thetas[1][i]) + 
-                mentor.a[3]*np.cos(thetas[1][i] + thetas[2][i]) - 
-                6*np.sin(thetas[1][i] + thetas[2][i] + thetas[3][i]), 2))*(time/1000)
-
-            print(abs(dist_4) + abs(dist_3) + abs(dist_2) + abs(dist_1))
-
-        
+        index = np.min([angle_1.shape[0], angle_2.shape[0], angle_3.shape[0], angle_4.shape[0], angle_5.shape[0]])
+        angles = np.transpose([angle_1[0:index], angle_2[0:index], angle_3[0:index], angle_4[0:index], angle_5[0:index]])
+        for angle in angles:
+            pos, rot = mentor.get_position(angle, 6)
+            points.append(pos[0:3])
+        self.points = points
+        self.dist = integration(np.array(points))
